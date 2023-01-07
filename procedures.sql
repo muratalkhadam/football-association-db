c4e54c1b7f75239f7f09c326ab05537730ef09e7
muratalkhadam <muratalkhadam@gmail.com>
2023-01-08 00:49:36 +0200

    procedures added

---
+++ procedures.sql
@@ -0,0 +1,54 @@
+USE football_association;
+
+
+DROP PROCEDURE IF EXISTS get_team_goals_in_league;
+DELIMITER $$
+CREATE PROCEDURE get_team_goals_in_league(IN team_id INT, IN league_id INT)
+BEGIN
+    SELECT IF(SUM(m.home_score + m.away_score) IS NOT NULL,
+              SUM(m.home_score + m.away_score),
+              0) AS goals
+    FROM matches m
+             JOIN teams t ON (t.id = m.home_team OR t.id = m.away_team)
+             JOIN leagues l ON l.id = m.league
+    WHERE (t.id = team_id AND l.id = league_id);
+END $$
+DELIMITER ;
+CALL get_team_goals_in_league(2, 2);
+
+DROP PROCEDURE IF EXISTS get_players_with_valid_contract;
+DELIMITER $$
+CREATE PROCEDURE get_players_with_valid_contract(IN team_id INT)
+BEGIN
+    SELECT t.team_name,
+           CONCAT(p.first_name, ' ', p.last_name) AS fullname
+    FROM teams t
+             JOIN contracts c on t.id = c.team
+             JOIN players p on p.id = c.player
+    WHERE t.id = team_id
+      AND end_date > CURDATE();
+END $$
+DELIMITER ;
+CALL get_players_with_valid_contract(1);
+
+
+DROP PROCEDURE IF EXISTS update_team_salaries;
+DELIMITER $$
+CREATE PROCEDURE update_team_salaries(IN team_id INT, IN percentage_to_change DECIMAL(5, 2), IN is_increase BOOLEAN)
+BEGIN
+    UPDATE contracts c
+    SET c.salary = (1 + (2 * is_increase - 1) * (percentage_to_change / 100)) * c.salary
+    WHERE c.team = team_id;
+END $$
+DELIMITER ;
+
+SELECT *
+FROM contracts
+WHERE team = 5;
+
+CALL update_team_salaries(5, 7, TRUE);
+
+SELECT *
+FROM contracts
+WHERE team = 5;
+
